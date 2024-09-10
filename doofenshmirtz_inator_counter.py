9187b223fcd80814af0a00b11a02af921bc0a6c1
nathanhatesphysics <nathan_alejandria@berkeley.edu>
2024-09-10 15:17:59 -0700

    Homework 1

---
+++ doofenshmirtz_inator_counter.py
@@ -0,0 +1,4 @@
+print(int(((1234567890*987654321)/9284692)+9365939825))
+# this is the amount of inators dr doofenshmirtz made
+# python3 doofenshmirtz_inator_counter.py
+# python3 Users/[username on computer]/Documents/PythonDeCal/doofenshmirtz_inator_counter.py
